90a400f3ccb345a4d63ebc6d503ac40976ac7549
Rizal Muhaimin <muhammad.rizal@dansmultipro.com>
2024-06-20 21:20:49 +0700

    add pgm "create some table"

---
+++ migrations/1718893027429_create-table-playlist-activities.js
@@ -0,0 +1,43 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('playlist_activities', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    action: {
+      type: 'VARCHAR(30)',
+      notNull: true,
+    },
+    time: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_activities');
+};
